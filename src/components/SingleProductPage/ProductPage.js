28a0b8899355e5ee513239342d854039abdb0598
trkline <trkline1@gmail.com>
2022-02-01 14:09:07 -0600

    working on single product page

---
+++ src/components/SingleProductPage/ProductPage.js
@@ -0,0 +1,21 @@
+import classes from "./ProductPage.module.css";
+import Button from "../ui/Button/Button";
+
+const ProductPage = (props) => {
+  let {product} = props;
+  return (
+    <div>
+      <img className={classes.productImage} src={product.imageURL} />
+      <h4 className={classes.title}>{product.title}</h4>
+      <div className={classes.description} >
+        <p className={classes.productDescription}>{product.description}</p>
+        <div className={classes.priceline}>
+          <p className={classes.price}>{product.price}</p>
+          <p className={classes.rating}>{product.rating}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
